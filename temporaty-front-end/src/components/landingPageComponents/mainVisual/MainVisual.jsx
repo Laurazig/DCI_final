3f671d0eeb4b5a1893f29a32563578d6a17cef48
F.Lora <federicalora94@gmail.com>
2022-07-19 12:00:19 +0200

    test push

--- temporaty-front-end/src/components/landingPageComponents/mainVisual/MainVisual.jsx
+++ temporaty-front-end/src/components/landingPageComponents/mainVisual/MainVisual.jsx
@@ -5,7 +5,7 @@ import "./mainVisual.scss"
 export default function MainVisual() {
   return (
     <div className='mainVisual'>
-        <h1>Healthy cooking</h1>
+        <h1>Healthy super cooking</h1>
         <h2>Made easy</h2>
         <button>Get started</button>
     </div>
