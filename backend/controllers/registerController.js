7b72bfbbbf091e4788869765f25153c651f44268
Sameer Ban <bansameer88@gmail.com>
2022-08-08 12:22:53 +0200

    after cleaning codes

--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -4,47 +4,47 @@ import User from "../models/user.js";
 
 
 export const registerController = async (req, res, next) => {
-    // console.log(`now working - console.log req.body ${req.body}`)
-    const { firstName, lastName, email, password, confirmPassword, year, month, day, street, houseNo, zipCode, city } = req.body;
+
+    const {firstName, lastName, email, password, confirmPassword, year, month, day, street, houseNo, zipCode, city} = req.body;
 
     let foundCustomer;
-    try {
-        foundCustomer = await User.findOne({ email: email });
-    } catch {
-        return next(createError(500, "could not query database. please try again. **registerController.js line14** "));
-    }
+    try{
+        foundCustomer = await User.findOne({email: email});
+        }catch {
+            return next(createError(500, "could not query database. please try again."));
+        }
 
     // If there is no customer in the collections with the username received from the frontend
     if (!foundCustomer) {
         // Create a new user based on data received from req.body
         const newCustomer = new User({
 
-            firstName: firstName,
-            lastName: lastName,
-            email: email,
-            password: password,
-            confirmPassword: confirmPassword,
-            year: year,
-            month: month,
-            day: day,
-            street: street,
-            houseNo: houseNo,
-            zipCode: zipCode,
-            city: city
-        //   meals: [],
-            /*  orders: [] */
+          firstName: firstName,
+          lastName: lastName,
+          email: email,
+          password: password,
+          confirmPassword: confirmPassword,
+          year: year,
+          month: month,
+          day: day,
+          street: street,
+          houseNo: houseNo,
+          zipCode: zipCode,
+          city: city
+          // meals:[],
+           /*  orders: [] */
 
         });
-        console.log(`console log registerController newCustomer ${newCustomer}`)
-        try {
-            await newCustomer.save();
-        } catch {
-            return next(createError(500, "couldn't create user. please try again! **registerController.js line42**"));
-        }
+console.log(newCustomer)
+       try {
+        await newCustomer.save();
+       } catch {
+        return next(createError(500, "couldn't create user. please try again!"));
+       } 
         res.status(201).json(newCustomer/* ._id */);
-
+   
     } else {
-        return next(createError(409, "Sorry, this username has been taken. Please choose another! **registerController.js line47**"));
-    }
+        return next(createError(409, "Sorry, this username has been taken. Please choose another!"));
+    }    
 }
 export default registerController;
