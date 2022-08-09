f99e96583839846d92dd9a2d24ad30d39f05b9e6
laurazig <lhzig@protonmail.com>
2022-08-09 16:25:18 +0200

    who has what rights

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -2,20 +2,18 @@ import express from "express";
 import { deleteAccount, deleteAllMeals, deleteSingleMeal, getMealsData, updatedMeals, updatedOrder } from "../controllers/usersController.js";
 
 const router = express.Router();
+//3 levels- 1 standard users / 2 premium users / 3 admin (Laura feels we don't need this)
 
-router.get("/:id");
+router.get("/:id", getMealsData); //1,2,3
 
-router.get("/:id", getMealsData);
+router.patch("/:id/meals", updatedMeals);//1,2,3
 
-router.patch("/:id/meals", updatedMeals);
+router.delete("/:id/meals", deleteAllMeals); // 3 (if this is the whole company meal database)
 
-router.delete("/:id/meals", deleteAllMeals); //users with premium |employees
+router.delete("/:id/meals/:mealId", deleteSingleMeal); //1,2,3
 
-router.delete("/:id/meals/:mealId", deleteSingleMeal);
+router.delete("/:id", deleteAccount);// 1,2,3
 
-router.delete("/:id", deleteAccount);
-
-router.patch("/:id/orders", updatedOrder);
+router.patch("/:id/orders", updatedOrder);  //1,2,3
 
 export default router;
-
