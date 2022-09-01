a8adc53a62ad7637d985b06575e24a864618ad0c
laurazig <lhzig@protonmail.com>
2022-09-01 12:17:06 +0200

    simply formatted stripe success & cancel pages

--- front-end/src/views/stripe/stripeCancel.js
+++ front-end/src/views/stripe/stripeCancel.js
@@ -1,10 +1,12 @@
-import React from 'react'
+import React from 'react';
+import "./stripe.scss";
 
 const StripeCancel = () => {
     return (
-        <div>
+        <div className='paymentBox'>
             <h2>Payment Cancelled </h2>
-            <h3>Please check your bank account and then  try again. Contact our customer services team if you have any questions</h3>
+            <h3>Please check your bank account and then  try again.</h3>
+            <h3>Contact our customer services team if you have any questions</h3>
         </div>
     )
 }
