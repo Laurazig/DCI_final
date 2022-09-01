d366d865bd18ce76d9b56175f851a89aba7edb84
F.Lora <federicalora94@gmail.com>
2022-09-01 12:47:17 +0200

    Almost finished how it works
    still to do: rotate arrow when on mobile

--- front-end/src/views/howitworks/HowItWorksPage.jsx
+++ front-end/src/views/howitworks/HowItWorksPage.jsx
@@ -1,36 +1,91 @@
-import React from 'react'
-import "./howItWorks.scss"
+import React, { useState, useContext } from "react";
+import "./howItWorks.scss";
 
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 
 const HowItWorksPage = () => {
+  const [hideFAQ, setHideFAQ] = useState(false);
+
+  const handleCliclFAQ = (event) => {
+    setHideFAQ((current) => !current);
+  };
+
   return (
-    <div className='howItWorksPage'>
-
-      <h1>How It Works</h1>
-
-      <div className='stepOne'>
-      <h3>1. Choose 3 meals</h3>
-      <p>login and select your 3 meals from the long list. On the Cart page enter your card details and click to confirm order. Easy peasy!</p>
+    <div className="howItWorksPage">
+      <div className="visualHowItWorks">
+        <h1>
+          How it
+          <br /> works
+        </h1>
       </div>
 
-      <h3>2. Get your delivery</h3>
-      <p>Each week, you’ll open simple step-by-step recipes complete with nutritional information and fresh, pre-measured ingredients to get you whipping up delicious dinners in no time.</p>
+      <div className="pWrapper">
+        <span>
+          <h3>1. Choose 3 meals</h3>
+          <p>
+            login and select your 3 meals from the long list. On the Cart page
+            enter your card details and click to confirm order. Easy peasy!
+          </p>
+        </span>
+        <ArrowForwardIosIcon />
+        <span>
+          <h3>2. Get your delivery</h3>
+          <p>
+            Each week, you’ll open simple step-by-step recipes complete with
+            nutritional information and fresh, pre-measured ingredients to get
+            you whipping up delicious dinners in no time.
+          </p>
+        </span>
+        <ArrowForwardIosIcon />
 
-      <h3>3. Cook, eat, enjoy!</h3>
-      <p>The old “what do you want to eat?” conversation is about to be banished from your life. Welcome to a world where dinner is always planned, simple, and delicious.</p>
-      
+        <span>
+          <h3>3. Cook, eat, enjoy!</h3>
+          <p>
+            The old “what do you want to eat?” conversation is about to be
+            banished from your life. Welcome to a world where dinner is always
+            planned, simple, and delicious.
+          </p>
+        </span>
+      </div>
 
-      <h2>Frequently Asked Questions</h2>
-      <p><strong>Can I select my meals?</strong></p>
-      <p>Yes! You can choose your meals every week.</p>
+      <span className="FAQtitle" onClick={handleCliclFAQ}>
+        <h2>Frequently Asked Questions</h2>
+        <KeyboardArrowDownIcon />
+      </span>
 
-      <p><strong>How will my food stay cool?</strong></p>
-      <p>We carefully handpack all ingredients with special ice packs and insulation, so your food keeps cool until you get home.</p>
+      <div className={hideFAQ === true ? "hide" : "FAQwrapper"}>
+        
 
-      <p><strong>What if I don't want a delivery every week?</strong></p>
-      <p>No worries. You can easily skip a week (or several!) when you need to. Just be sure to do so by your cutoff time 5 days prior to your delivery.</p>
+        <span>
+          <p>
+            <strong>Can I select my meals?</strong>
+          </p>
+          <p>Yes! You can choose your meals every week.</p>
+        </span>
+
+        <span>
+          <p>
+            <strong>How will my food stay cool?</strong>
+          </p>
+          <p>
+            We carefully handpack all ingredients with special ice packs and
+            insulation, so your food keeps cool until you get home.
+          </p>
+        </span>
+        <span>
+          <p>
+            <strong>What if I don't want a delivery every week?</strong>
+          </p>
+          <p>
+            No worries. You can easily skip a week (or several!) when you need
+            to. Just be sure to do so by your cutoff time 5 days prior to your
+            delivery.
+          </p>
+        </span>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default HowItWorksPage
+export default HowItWorksPage;
