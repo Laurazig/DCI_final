1763e61a2e85695dffe93d4abd332a940b2c7796
laurazig <lhzig@protonmail.com>
2022-09-03 12:24:21 +0200

    filled button hover

--- front-end/src/components/landingPageComponents/mainVisual/MainVisual.jsx
+++ front-end/src/components/landingPageComponents/mainVisual/MainVisual.jsx
@@ -1,20 +1,17 @@
 import React from 'react'
 import { NavLink } from "react-router-dom";
-
 import "./mainVisual.scss"
-
 //import mainVisualPhoto from '../../../assets/mainVisualAlternative.jpg'
-
 
 export default function MainVisual() {
   return (
     <div className='mainVisual'>
-        <div className='titleAndButton'>
-        <h1><span className='h1Top'>bio cooking</span> <br/>Made easy</h1>
+      <div className='titleAndButton'>
+        <h1><span className='h1Top'>bio cooking</span> <br />Made easy</h1>
         <NavLink to="howitworks">
-        <button>Get started</button>
+          <button>Get started</button>
         </NavLink>
-        </div>
+      </div>
     </div>
   )
 }
