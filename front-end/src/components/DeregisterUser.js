3669ce5d35b4d4b400cae7de1cf81dfdf5c147f0
laurazig <lhzig@protonmail.com>
2022-09-03 17:32:04 +0200

    admin buttons styling

--- front-end/src/components/DeregisterUser.js
+++ front-end/src/components/DeregisterUser.js
@@ -1,11 +1,12 @@
 import React, {useContext} from 'react';
 import  { MyContext }  from "../App.js";
+import './admin.scss'
 
 const DeregisterUser = () => {
   const { deleteUserAccount } = useContext(MyContext);
   return (
     <div>
-      <button onClick={deleteUserAccount}> Delete user Account</button>
+      <button onClick={deleteUserAccount} > Delete user account</button>
     </div>
   )
 }
